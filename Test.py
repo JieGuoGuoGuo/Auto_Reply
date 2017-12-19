2ea0d1b1732609884539293ffe037bda51fba712
PanJie <546445830@qq.com>
2017-12-19 09:49:49 +0800

    1. add bat to open the programm directly 2. add the Interface to receive the red packet

--- Test.py
+++ Test.py
@@ -19,18 +19,19 @@ face_bug=None  #针对表情包的内容
 @itchat.msg_register([TEXT, PICTURE, FRIENDS, CARD, MAP, SHARING, RECORDING, ATTACHMENT, VIDEO],isFriendChat=True, isGroupChat=True, isMpChat=True)
 def handle_receive_msg(msg):
     global face_bug
-    msg_time_rec = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())   #接受消息的时间
+    #接受消息的时间
+    msg_time_rec = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())   
 
     #在好友列表中查询发送信息的好友昵称
     msg_from = itchat.search_friends(userName=msg['FromUserName'])
     if 'NickName' in msg_from:
         msg_from = msg_from['NickName'] 
-
     # msg_from = itchat.search_friends(userName=msg['FromUserName'])['NickName']   
-    msg_time = msg['CreateTime']    #信息发送的时间
-    msg_id = msg['MsgId']    #每条信息的id
-    msg_content = None      #储存信息的内容
-    msg_share_url = None    #储存分享的链接，比如分享的文章和音乐
+
+    msg_time        = msg['CreateTime']     #信息发送的时间
+    msg_id          = msg['MsgId']          #每条信息的id
+    msg_content     = None                  #储存信息的内容
+    msg_share_url   = None                  #储存分享的链接，比如分享的文章和音乐
     # print('handle_receive_msg ---- >Log Test : Begin')
     # print (msg)
     # print (msg_time_rec)
@@ -38,7 +39,9 @@ def handle_receive_msg(msg):
     # print (msg['MsgId'])
     # print (msg['User']['RemarkName'])
     # print('handle_receive_msg ---- >Log Test : End')
-    if msg['Type'] == 'Text' or msg['Type'] == 'Friends':     #如果发送的消息是文本或者好友推荐
+
+    # 1. 如果发送的消息是文本或者好友推荐
+    if msg['Type'] == 'Text' or msg['Type'] == 'Friends':     
         msg_content = msg['Text']
 
         # 获取对方的名字(如果有备注的话则记录备注)
@@ -58,31 +61,39 @@ def handle_receive_msg(msg):
         
         print (msg_content)
 
-    #如果发送的消息是附件、视屏、图片、语音
+    # 2. 如果发送的消息是附件、视屏、图片、语音
     elif msg['Type'] == "Attachment" or msg['Type'] == "Video" \
             or msg['Type'] == 'Picture' \
             or msg['Type'] == 'Recording':
         msg_content = msg['FileName']    #内容就是他们的文件名
         msg['Text'](str(msg_content))    #下载文件
         # print msg_content
-    elif msg['Type'] == 'Card':    #如果消息是推荐的名片
-        msg_content = msg['RecommendInfo']['NickName'] + '的名片'    #内容就是推荐人的昵称和性别
+
+    # 3. 如果消息是推荐的名片
+    elif msg['Type'] == 'Card':  
+        # 内容就是推荐人的昵称和性别
+        msg_content = msg['RecommendInfo']['NickName'] + '的名片'    
         if msg['RecommendInfo']['Sex'] == 1:
             msg_content += '性别为男'
         else:
             msg_content += '性别为女'
 
         print (msg_content)
-    elif msg['Type'] == 'Map':    #如果消息为分享的位置信息
+
+    # 4. 如果消息为分享的位置信息
+    elif msg['Type'] == 'Map': 
         x, y, location = re.search(
             "<location x=\"(.*?)\" y=\"(.*?)\".*label=\"(.*?)\".*", msg['OriContent']).group(1, 2, 3)
         if location is None:
             msg_content = r"纬度->" + x.__str__() + " 经度->" + y.__str__()     #内容为详细的地址
         else:
             msg_content = r"" + location
-    elif msg['Type'] == 'Sharing':     #如果消息为分享的音乐或者文章，详细的内容为文章的标题或者是分享的名字
+
+    # 5. 如果消息为分享的音乐或者文章，详细的内容为文章的标题或者是分享的名字
+    elif msg['Type'] == 'Sharing':
         msg_content = msg['Text']
-        msg_share_url = msg['Url']       #记录分享的url
+        # 记录分享的url
+        msg_share_url = msg['Url']       
         print (msg_share_url)
     face_bug=msg_content
 
@@ -98,7 +109,7 @@ def handle_receive_msg(msg):
     )
 
 
-##这个是用于监听是否有消息撤回
+# 监听是否有消息撤回
 @itchat.msg_register(NOTE, isFriendChat=True, isGroupChat=True, isMpChat=True)
 def information(msg):
     print('----------------------------- >')
@@ -133,18 +144,29 @@ def information(msg):
             # 删除字典旧消息
             msg_information.pop(old_msg_id)
 
+# 监听群里是否有人@自己
 @itchat.msg_register(TEXT, isGroupChat=True)
 def text_reply(msg):
     if msg.isAt:
         msg.user.send(u'@%s\u2005I received: %s' % (
             msg.actualNickName, msg.text))
 
+
+# 红包
+@itchat.msg_register([NOTE])
+def text_reply(msg):
+    print('---------------------------------------> ')
+    print(msg)
+
+
+
+#########################################
 itchat.auto_login(hotReload=True)
 Main.main()
 itchat.run()
 
+# 多开
 # newInstance = itchat.new_instance()
 # newInstance.auto_login(hotReload=True, statusStorageDir='newInstance.pkl')
-
 # newInstance.run()
 
